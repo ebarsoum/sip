9fc612b8254fb3b54bc53f9ad4f57174072cccc4
Emad Barsoum <emad.barsoum@sizesnap.com>
2013-07-25 23:52:03 -0700

    Add main entry to SIP compiler. Spew AST string presentation into a file. Update makefile with sip.ml.

--- src/sip.ml
+++ src/sip.ml
@@ -1,14 +1,29 @@
-type action = Ast | Compile
+open Printf
 
-let _ =
-  let action = if Array.length Sys.argv > 1 then
-    List.assoc Sys.argv.(1) [ ("-a", Ast);
-			                  ("-c", Compile) ]
-  else Compile in
-  let lexbuf = Lexing.from_channel stdin in
-  let program = Parser.program Scanner.token lexbuf in
-  match action with
-    Ast -> let listing = Ast.string_of_program program
-           in print_string listing
-  | Compile -> Execute.execute_prog (Compile.translate program)
- 
+type action = Ast | Compile | None
+
+let out_ast_file = "a.ast"
+let out_cc_file  = "a.cc"
+let out_cl_file  = "a.cl"
+
+let fwrite name content =
+	let out = open_out name in
+	  fprintf out "%s\n" content;
+	  close_out out
+
+let main () =
+  let action = 
+    if Array.length Sys.argv > 2 then
+      List.assoc Sys.argv.(1) [ ("-a", Ast);
+                                ("-c", Compile) ]
+    else None in
+    let lexbuf = Lexing.from_channel (open_in Sys.argv.(2)) in
+    let program = Parser.program Scanner.token lexbuf in
+    match action with
+      Ast -> let listing = Ast.string_of_program program in
+               fwrite out_ast_file listing
+    | Compile -> fwrite out_cc_file "test"
+    | None -> print_string "None\n"
+
+let _ = main ()
+
